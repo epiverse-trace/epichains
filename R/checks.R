b5cd54d20571007fcdce5aeba965db193f243c80
jamesaazam <james.azam@lshtm.ac.uk>
2023-06-13 11:58:28 +0100

    Removed redundant roxygen tags

--- R/checks.R
+++ R/checks.R
@@ -1,11 +1,7 @@
 #' Check if offspring argument is specified as a character string
 #'
 #' @param offspring
-#'
-#' @return
-#' @export
 #' @keywords internal
-#' @examples
 check_offspring_valid <- function(offspring) {
   if (!is.character(offspring)) {
     stop(sprintf(
@@ -20,11 +16,7 @@ check_offspring_valid <- function(offspring) {
 #' Check if constructed random number generator for offspring exists
 #'
 #' @param roffspring_name
-#'
-#' @return
-#' @export
-#'
-#' @examples check_offspring_exists("rpois")
+#' @keywords internal
 check_offspring_func_valid <- function(roffspring_name) {
   if (!(exists(roffspring_name)) || !is.function(get(roffspring_name))) {
     stop("Function ", roffspring_name, " does not exist.")
@@ -36,10 +28,7 @@ check_offspring_func_valid <- function(roffspring_name) {
 #'
 #' @param serials_sampler
 #'
-#' @return
-#' @export
 #' @keywords internal
-#' @examples
 check_serial_valid <- function(serials_sampler) {
   if (!is.function(serials_sampler)) {
     stop(sprintf(
@@ -51,6 +40,11 @@ check_serial_valid <- function(serials_sampler) {
 }
 
 
+#' Check that nchains is greater than 0 and not infinite
+#'
+#' @param nchains
+#'
+#' @keywords internal
 check_nchains_valid <- function(nchains) {
   if (nchains < 1 || is.infinite(nchains)) {
     stop("`nchains` must be > 0 but less than `Inf`")
