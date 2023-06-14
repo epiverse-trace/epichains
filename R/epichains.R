30d9b318ffc6878701fd95dfa9c765507483866b
jamesaazam <james.azam@lshtm.ac.uk>
2023-06-14 15:29:05 +0100

    Cleaned up documentation of the head and tail methods

--- R/epichains.R
+++ R/epichains.R
@@ -145,6 +145,7 @@ is_epichains <- function(x) {
 #'
 #' @return Checks if an object is of class `epichains` and if so
 #' checks that it's in the right format as a "data.frame" or vector.
+#' @keywords internal
 validate_epichains <- function(x) {
   if (!is_epichains(x)) {
     stop("Object must have an epichains class")
@@ -176,21 +177,23 @@ validate_epichains <- function(x) {
   invisible(x)
 }
 
-#' `head` and `tail` methods for [`epichains`] class
+#' `head` method for [`epichains`] class
 #'
 #' @param x An [`epichains`] object
 #' @param ... further arguments passed to or from other methods
-#'
-#' @return object of class `data.frame`
-#' @export
-#'
 #' @importFrom utils head
-#' @importFrom utils tail
+#' @return object of class `data.frame`
+#' @author James M. Azam
+#' @export
 head.epichains <- function(x, ...) {
   utils::head(as.data.frame(x), ...)
 }
 
-#' @rdname head.epichains
+#' `tail` method for [`epichains`] class
+#' @param x An [`epichains`] object
+#' @param ... further arguments passed to or from other methods
+#' @importFrom utils tail
+#' @author James M. Azam
 #' @export
 tail.epichains <- function(x, ...) {
   utils::tail(as.data.frame(x), ...)
