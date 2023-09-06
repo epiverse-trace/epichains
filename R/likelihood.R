5f052c6adea3eca327cd4fd97a8dec13dc35ddf2
jamesaazam <james.azam@lshtm.ac.uk>
2023-09-06 19:36:00 +0100

    Improve error message

--- R/likelihood.R
+++ R/likelihood.R
@@ -37,7 +37,9 @@ likelihood <- function(chains, statistic = c("size", "length"), offspring_dist,
   ## checks
   check_offspring_valid(offspring_dist)
 
-  if (obs_prob <= 0 || obs_prob > 1) stop("'obs_prob' must be within (0,1]")
+  if (obs_prob <= 0 || obs_prob > 1) {
+    stop("'obs_prob' is a probability and must be between 0 and 1 inclusive")
+    }
   if (obs_prob < 1) {
     if (missing(nsim_obs)) {
       stop("'nsim_obs' must be specified if 'obs_prob' is < 1")
