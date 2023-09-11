6cb3ec2556e0d6f0217f840b4b64c9240edbdf19
jamesaazam <james.azam@lshtm.ac.uk>
2023-09-11 19:47:10 +0100

    Fixed linting issues with the tests

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,9 +1,9 @@
 test_that("Helper functions work correctly", {
-  expect_equal(
+  expect_identical(
     construct_offspring_ll_name(
       offspring_dist = "pois",
       chain_statistic = "size"
-      ),
+    ),
     "pois_size_ll"
   )
 })
