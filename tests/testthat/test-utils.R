1f713dc4fca314a4ec526378491e1e980ffc71a0
jamesaazam <james.azam@lshtm.ac.uk>
2023-09-14 20:10:13 +0100

    Add tests for utils.R

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,120 @@
+test_that("Reparametrized distributions work", {
+  expect_length(
+    rnbinom_mean_disp(
+      n = 5,
+      mn = 4,
+      disp = 2
+      ),
+    5
+  )
+})
+
+test_that("Log-probabilities work", {
+  expect_length(
+    complementary_logprob(x = 0),
+    1
+  )
+  expect_length(
+    complementary_logprob(x = -Inf),
+    1
+  )
+  expect_length(
+    complementary_logprob(x = -0.1),
+    1
+  )
+})
+
+test_that("Chain lengths sampler works", {
+  expect_length(
+    rgen_length(
+      n = 1,
+      x = c(1, 2, 3),
+      prob = 0.3
+    ),
+    3
+  )
+})
+
+test_that("Chain sizes sampler works", {
+  expect_length(
+    rbinom_size(
+      n = 1,
+      x = c(1, 2, 3),
+      prob = 0.3
+    ),
+    3
+  )
+})
+
+test_that("Reparametrized distributions are numerically correct", {
+  set.seed(12)
+  expect_identical(
+    rnbinom_mean_disp(
+      n = 5,
+      mn = 4,
+      disp = 2
+      ),
+    c(0, 2, 5, 2, 3)
+  )
+})
+
+test_that("Log-probabilities are numerically correct", {
+  expect_identical(
+    complementary_logprob(x = 0),
+    -Inf
+  )
+  expect_identical(
+    complementary_logprob(x = -Inf),
+    0
+  )
+  expect_lt(
+    complementary_logprob(x = -0.1),
+    0
+  )
+})
+
+test_that("Chain lengths sampler is numerically correct", {
+  set.seed(12)
+  expect_identical(
+    rgen_length(
+      n = 1,
+      x = c(1, 2, 3),
+      prob = 0.3
+    ),
+    c(8, 9, 10)
+  )
+})
+
+test_that("Chain sizes sampler is numerically correct", {
+  set.seed(12)
+  expect_identical(
+    rbinom_size(
+      n = 1,
+      x = c(1, 2, 3),
+      prob = 0.3
+    ),
+    c(1, 2, 3)
+  )
+})
+
+test_that("Reparametrized distributions throw warnings", {
+  expect_warning(
+    rnbinom_mean_disp(
+      n = 5,
+      mn = 4,
+      disp = 0.9
+    ),
+    "NAs produced"
+  )
+})
+
+test_that("Log-probabilities throw warnings", {
+  expect_warning(
+    complementary_logprob(0.1),
+    "NaNs produced"
+  )
+  expect_warning(
+    complementary_logprob(Inf),
+    "NaNs produced"
+  )
+})
