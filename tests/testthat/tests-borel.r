b1cfea4967784d9b5b04671b6a2c14f9c8b8ab37
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-01-26 15:09:52 +0000

    add test

--- tests/testthat/tests-borel.r
+++ tests/testthat/tests-borel.r
@@ -3,6 +3,7 @@ test_that("We can calculate probabilities and sample", {
   expect_gt(dborel(1, 0.5), 0)
   expect_identical(dborel(1, 0.5, log = TRUE), -0.5)
   expect_length(rborel(2, 0.9), 2)
+  expect_length(rgborel(2, 0.5, 0.9), 2)
 })
 
 test_that("Errors are thrown", {
