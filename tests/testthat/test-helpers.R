d22fd51e9c741061ab89d5107a1477d45761fb5b
jamesaazam <james.azam@lshtm.ac.uk>
2024-02-06 13:47:50 +0000

    Test for errors when switch statement does not match

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -17,6 +17,14 @@ test_that("update_chain_stat works correctly", {
     ),
     stat_latest + pmin(1, n_offspring)
   )
+  expect_error(
+    .update_chain_stat(
+      stat_type = "foo",
+      stat_latest = stat_latest,
+      n_offspring = n_offspring
+    ),
+    "stat_type must be 'size' or 'length'"
+  )
 })
 
 test_that("get_statistic_func works correctly", {
@@ -27,5 +35,9 @@ test_that("get_statistic_func works correctly", {
   expect_identical(
     .get_statistic_func(chain_statistic = "length"),
     rgen_length
+  )
+  expect_error(
+    .get_statistic_func(chain_statistic = "foo"),
+    "chain_statistic must be 'size' or 'length'"
   )
 })
