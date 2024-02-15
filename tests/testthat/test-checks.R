28bdc4f95614b1d8521f3890d964cdaef3345f9b
James Azam <james.m.azam@gmail.com>
2024-02-15 17:15:48 +0000

    Restructure tests to use a default function

--- tests/testthat/test-checks.R
+++ tests/testthat/test-checks.R
@@ -1,175 +1,139 @@
-test_that("Checks work", {
-  expect_error(
-    .check_offspring_func_valid(rrpois),
-    "not found"
-  )
-  expect_error(
-    .check_generation_time_valid("a"),
-    "Must be a function"
-  )
-  expect_error(
-    .check_generation_time_valid(function(x) rep("a", 10)),
-    "numeric"
-  )
-  expect_error(
-    .check_generation_time_valid(function(x) 3),
-    "Must have length"
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_chains",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1,
-      generation_time = NULL,
-      t0 = 0,
-      tf_specified = TRUE,
-      tf = 10
-    ),
-    "If `tf` is specified, `generation_time` must be specified too."
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_summary",
-      index_cases = 0,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1
-    ),
-    "Must be >= 1."
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_summary",
-      index_cases = 10,
-      statistic = "a",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1
-    ),
-    "Must be element of set"
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_summary",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = r,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1
-    ),
-    "object 'r' not found"
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_summary",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = -1,
-      pop = 10,
-      percent_immune = 0.1
-    ),
-    "Element 1 is not >= 0"
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_summary",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 1,
-      pop = -1,
-      percent_immune = 0.1
-    ),
-    "Element 1 is not >= 1"
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_summary",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 1,
-      pop = 10,
-      percent_immune = -0.1
-    ),
-    "Element 1 is not >= 0"
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_chains",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1,
-      generation_time = NULL,
-      t0 = 0,
-      tf_specified = TRUE,
-      tf = 10
-    ),
-    "If `tf` is specified, `generation_time` must be specified too."
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_chains",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1,
-      generation_time = function(x) rep(3, 10),
-      t0 = -1,
-      tf_specified = TRUE,
-      tf = Inf
-    ),
-    "Element 1 is not >= 0."
-  )
-  expect_error(
-    .check_sim_args(
-      func_name = "simulate_chains",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1,
-      generation_time = function(x) rep(3, 10),
-      t0 = 0,
-      tf_specified = TRUE,
-      tf = -1
-    ),
-    "Element 1 is not >= 0."
-  )
-  expect_no_error(
-    .check_sim_args(
-      func_name = "simulate_chains",
-      index_cases = 10,
-      statistic = "size",
-      offspring_dist = rpois,
-      stat_max = 10,
-      pop = 10,
-      percent_immune = 0.1,
-      generation_time = function(x) rep(3, 10),
-      t0 = 0,
-      tf_specified = TRUE,
-      tf = 10
+# Default function to check wrongly specified arguments and return an error
+# message
+.check_sim_args_default <- function(...) {
+    default_args <- list(
+        func_name = "simulate_chains",
+        index_cases = 10,
+        statistic = "size",
+        offspring_dist = rpois,
+        stat_max = 10,
+        pop = 10,
+        percent_immune = 0.1,
+        generation_time = function(x) rep(3, 10),
+        t0 = 0,
+        tf_specified = TRUE,
+        tf = 10
     )
-  )
-  expect_no_error(
-    .check_statistic_args(
-      statistic = "size",
-      stat_max = 10
+    # Modify the default arguments with the user's arguments
+    new_args <- modifyList(
+        default_args,
+        list(...)
     )
-  )
+    # Run the check_sim_args function and capture the output
+    out <- tryCatch(
+        expr = {
+            do.call(
+                .check_sim_args,
+                new_args
+            )
+        },
+        error = function(e) {
+            stop(e)
+        }
+    )
+    # Return the output
+    return(out)
+}
+
+test_that("Smaller checker functions work", {
+    expect_error(
+        .check_offspring_func_valid(rrpois),
+        "not found"
+    )
+    expect_error(
+        .check_generation_time_valid("a"),
+        "Must be a function"
+    )
+    expect_error(
+        .check_generation_time_valid(function(x) rep("a", 10)),
+        "numeric"
+    )
+    expect_error(
+        .check_generation_time_valid(function(x) 3),
+        "Must have length"
+    )
+    expect_no_error(
+        .check_statistic_args(
+            statistic = "size",
+            stat_max = 10
+        )
+    )
+})
+
+test_that(".check_sim_args() returns errors", {
+    expect_no_error(
+        .check_sim_args_default()
+    )
+    # index_cases must be >= 1
+    expect_error(
+        .check_sim_args_default(
+            index_cases = 0
+        ),
+        "Must be >= 1."
+    )
+    # statistic can only be "size" or "length"
+    expect_error(
+        .check_sim_args_default(
+            statistic = "duration"
+        ),
+        "Must be element of set \\{'size','length'\\}"
+    )
+    # offspring_dist must be a function
+    expect_error(
+        .check_sim_args_default(
+            offspring_dist = "rpois"
+        ),
+        "Must be a function, not 'character'"
+    )
+    # offspring_dist must be a known function (in the environment)
+    expect_error(
+        .check_sim_args_default(
+            offspring_dist = r
+        ),
+        "object 'r' not found"
+    )
+    # stat_max must be >= 1
+    expect_error(
+        .check_sim_args_default(
+            stat_max = 0
+        ),
+        "Assertion failed."
+    )
+    # pop cannot be negative
+    expect_error(
+        .check_sim_args_default(
+            pop = -1
+        ),
+        "Element 1 is not >= 1."
+    )
+    # percent_immune must be in between 0 and 1
+    expect_error(
+        .check_sim_args_default(
+            percent_immune = 1.1
+        ),
+        "Element 1 is not <= 1."
+    )
+    # t0 cannot be negative
+    expect_error(
+        .check_sim_args_default(
+            t0 = -1
+        ),
+        "Element 1 is not >= 0."
+    )
+    # tf cannot be negative
+    expect_error(
+        .check_sim_args_default(
+            tf = -1
+        ),
+        "Element 1 is not >= 0."
+    )
+    # If tf is specified, generation_time must be specified too
+    expect_error(
+        .check_sim_args_default(
+            generation_time = NULL,
+            tf_specified = TRUE,
+            tf = 10
+        ),
+        "If `tf` is specified, `generation_time` must be specified too."
+    )
 })
