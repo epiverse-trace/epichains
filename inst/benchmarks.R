03768598a48ae3c45011545d8e90239ce0f0260a
jamesaazam <james.azam@lshtm.ac.uk>
2024-03-05 15:15:25 +0000

    Linting

--- inst/benchmarks.R
+++ inst/benchmarks.R
@@ -1,24 +1,30 @@
-# Benchmark of two approaches for obtaining chain summaries
-# 1. Simulate chains with `simulate_chains()` then run `summary()` on the
-# output to obtain an `<epichains_summary>` object OR
-# 2. Run `simualte_summary()` directly with the same parameters as
-# `simulate_chains()`
+#' Benchmark of two approaches for obtaining chain summaries
+#' 1. Simulate chains with `simulate_chains()` then run `summary()` on the
+#' output to obtain an `<epichains_summary>` object OR
+#' 2. Run `simualte_summary()` directly with the same parameters as
+#' `simulate_chains()`
 
-# Load libraries
-library(epichains)
-library(bench)
+#' Load libraries
+devtools::load_all()
+library("bench")
 
-# Benchmark
+# Shared arguments
+shared_args <- list(
+    index_cases = 10,
+    statistic = "size",
+    offspring_dist = rnbinom,
+    stat_max = 1000,
+    mu = 2,
+    size = 0.2
+)
+
+#' Benchmark
 summary_benchmarks <- bench::mark(
     sim_chains = {
         set.seed(32)
-        out <- simulate_chains(
-            index_cases = 10,
-            statistic = "size",
-            offspring_dist = rnbinom,
-            stat_max = 1000,
-            mu = 2,
-            size = 0.2
+        out <- do.call(
+            simulate_chains,
+            shared_args
         )
         summary(out)
     },
@@ -37,6 +43,7 @@ summary_benchmarks <- bench::mark(
 
 bnmark_plt <- plot(bnmarks)
 
-ggplot2::ggsave("./inst/summary_benchmarks.png", bnmark_plt)
-
-
+ggplot2::ggsave(
+    file.path("inst", "summary_benchmarks.png"),
+    bnmark_plt
+)
