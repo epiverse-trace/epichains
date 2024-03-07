5f09f669a55d9fa6ddcf2b927287a0f2fdd31aa1
James Azam <james.m.azam@gmail.com>
2024-03-07 17:50:27 +0000

    Add tests

--- tests/testthat/test-likelihood.R
+++ tests/testthat/test-likelihood.R
@@ -89,6 +89,54 @@ test_that("Likelihoods can be calculated", {
   }
 )
 
+test_that("likelihood() works with epichains and epichains_summary objects", {
+    # Simulate an <epichains_tree> object
+    set.seed(32)
+    chains_tree_eg <- simulate_chains(
+      index_cases = 10,
+      pop = 100,
+      percent_immune = 0,
+      statistic = "size",
+      offspring_dist = rpois,
+      stat_max = 10,
+      generation_time = function(n) rep(3, n),
+      lambda = 0.9
+    )
+    # Simulate an <epichains_summary> object
+    set.seed(32)
+    chains_summary_eg <- simulate_summary(
+      index_cases = 10,
+      pop = 100,
+      percent_immune = 0,
+      statistic = "size",
+      offspring_dist = rpois,
+      stat_max = 10,
+      lambda = 0.9
+    )
+    # Use the simulated <epichains_tree> object to calculate likelihood
+    expect_equal(
+      likelihood(
+        chains = chains_tree_eg,
+        statistic = "size",
+        offspring_dist = rpois,
+        lambda = 0.9,
+        stat_max = 10
+      ),
+      -23.538996774
+    )
+    # Use the simulated <epichains_summary> object to calculate likelihood
+    expect_equal(
+      likelihood(
+        chains = chains_summary_eg,
+        statistic = "size",
+        offspring_dist = rpois,
+        lambda = 0.9,
+        stat_max = 10
+      ),
+      -23.538997
+    )
+})
+
 test_that("Likelihoods are numerically correct", {
   chains <- c(1, 1, 4, 7)
   set.seed(12)
@@ -191,6 +239,15 @@ test_that("Errors are thrown", {
       nsim_obs = 100,
       lambda = 0.5,
       log = "s"
+    ),
+    "Must be of type"
+  )
+  expect_error(
+    likelihood(
+      chains = as.factor(chains),
+      statistic = "size",
+      offspring_dist = rpois,
+      lambda = 0.5
     ),
     "Must be of type"
   )
