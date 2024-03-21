93d16204377e4ab77761c41133c567f35e24c28c
jamesaazam <james.azam@lshtm.ac.uk>
2024-03-21 14:35:43 +0000

    Use the new dot-prefixed names

--- tests/testthat/test-epichains.R
+++ tests/testthat/test-epichains.R
@@ -269,16 +269,16 @@ test_that("validate_epichains works", {
   )
   #' Expectations
   expect_invisible(
-    .validate_epichains_tree(susc_outbreak_raw)
+    .validate_epichains(susc_outbreak_raw)
   )
   expect_invisible(
-    .validate_epichains_tree(susc_outbreak_raw2)
+    .validate_epichains(susc_outbreak_raw2)
   )
   expect_invisible(
-    .validate_epichains_tree(tree_sim_raw)
+    .validate_epichains(tree_sim_raw)
   )
   expect_invisible(
-    .validate_epichains_tree(tree_sim_raw2)
+    .validate_epichains(tree_sim_raw2)
   )
   expect_invisible(
     .validate_epichains_summary(chain_summary_raw)
@@ -331,19 +331,19 @@ test_that("is_chains_tree works", {
   )
   #' Expectations
   expect_true(
-    .is_epichains_tree(susc_outbreak_raw)
+    .is_epichains(susc_outbreak_raw)
   )
   expect_true(
-    .is_epichains_tree(susc_outbreak_raw2)
+    .is_epichains(susc_outbreak_raw2)
   )
   expect_true(
-    .is_epichains_tree(tree_sim_raw)
+    .is_epichains(tree_sim_raw)
   )
   expect_true(
-    .is_epichains_tree(tree_sim_raw2)
+    .is_epichains(tree_sim_raw2)
   )
   expect_false(
-    .is_epichains_tree(chain_summary_raw)
+    .is_epichains(chain_summary_raw)
   )
 })
 
