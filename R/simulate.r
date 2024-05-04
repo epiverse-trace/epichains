7bd0adc7e260b22c68ef11896f98e1da9007acdf
James Azam <james.m.azam@gmail.com>
2024-05-04 14:53:22 +0100

    Give infectees unique IDs

--- R/simulate.r
+++ R/simulate.r
@@ -288,6 +288,7 @@ simulate_chains <- function(index_cases,
   # Post processing
   #   # sort by sim_id and infector_id
   rownames(tree_df) <- NULL
+  tree_df$infectee <- seq_len(nrow(tree_df))
   # We want to reorder the columns but that depends on whether "time" is
   # present or not, so we need to determine that first
   column_order <- if (missing(generation_time)) {
