0c3802b414ce4d27c9c2a00b4c3d18ef32ef6b23
jamesaazam <james.azam@lshtm.ac.uk>
2024-05-14 23:09:56 +0100

    Improve definition of the "chain" column

--- R/simulate.r
+++ R/simulate.r
@@ -51,7 +51,7 @@
 #' @param ... Parameters of the offspring distribution as required by R.
 #' @return An `<epichains>` object, which is basically a `<data.frame>`
 #' with columns:
-#' * `chain` - an ID for each chain in the simulation,
+#' * `chain` - an ID for active/ongoing chains,
 #' * `infectee` - a unique ID for each infectee.
 #' * `infector` - an ID for the infector of each infectee.
 #' * `generation` - a discrete time point during which infection occurs, and
