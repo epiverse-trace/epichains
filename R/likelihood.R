362ff54bac81bb5c3c9866c0227a7cf9afffefba
jamesaazam <james.azam@lshtm.ac.uk>
2024-05-28 17:51:34 +0100

    Remove warning before censoring epichains_summary objects internally

--- R/likelihood.R
+++ R/likelihood.R
@@ -162,21 +162,10 @@ likelihood <- function(chains, statistic = c("size", "length"), offspring_dist,
   if (is.finite(stat_threshold)) {
     # censor the chains to be at most stat_threshold
     chains <- pmin(chains, stat_threshold)
-  } else if (any(is.infinite(chains))) {
-    if (.is_epichains_summary(chains)) {
-      # <epichains_summary> objects can only contain Inf values if
-      # stat_threshold is finite in the simulation, so we'll use that
-      # value to censor the chains.
-      warning(
-        "`chains` must be censored with a finite value. ",
-        "Censoring with `stat_threshold` = ",
-        .get_stat_threshold(chains),
-        " as used in the simulation.",
-        call. = FALSE
-      )
-      stat_threshold <- .get_stat_threshold(chains)
-      chains <- pmin(chains, stat_threshold)
-    } else {
+  } else if (any(is.infinite(chains)) && !.is_epichains_summary(chains)) {
+      # Developer note: <epichains_summary> objects also pass the`is.numeric()`
+      # check, so we need to check if the object is not an <epichains_summary>
+      # object.
       # Numeric chains can only contain Inf values based on human error/decision
       # to censor it with infinite values, so we ask the user to fix that.
       stop(
@@ -184,7 +173,6 @@ likelihood <- function(chains, statistic = c("size", "length"), offspring_dist,
         "Replace the `Inf` values with a finite value.",
         call. = FALSE
       )
-    }
   }
 
   # Apply the observation process
