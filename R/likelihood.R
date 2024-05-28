9cef81fcf60ae4a8975bf94c8fc0f475301bc72a
jamesaazam <james.azam@lshtm.ac.uk>
2024-05-28 12:04:17 +0100

    Revert order of arguments

--- R/likelihood.R
+++ R/likelihood.R
@@ -98,7 +98,7 @@
 #' @export
 #nolint start: cyclocomp_linter
 likelihood <- function(chains, statistic = c("size", "length"), offspring_dist,
-                       nsim_obs, stat_threshold = Inf, obs_prob = 1, log = TRUE,
+                       nsim_obs, obs_prob = 1, stat_threshold = Inf, log = TRUE,
                        exclude = NULL, individual = FALSE, ...) {
   statistic <- match.arg(statistic)
 
